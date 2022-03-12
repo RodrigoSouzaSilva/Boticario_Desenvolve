60be3d5198d85f352f1a595369c86fead45ab26e
rodrigosouzasilva <rodrigo_souza.91@hotmail.com>
2022-03-12 13:45:23 -0300

    finalizando semana 7

---
+++ semana 7/js-arrays/callbacks.js
@@ -0,0 +1,7 @@
+let nomes = ['Ana', 'Ju', 'Leo', 'Paula'];
+
+nomes.forEach(imprimeNomes); 
+
+function imprimeNomes(nome, indice) { // indice opcional
+    console.log(nome);
+}
---
+++ semana 7/js-arrays/reduce-media-geral.js
@@ -0,0 +1,24 @@
+const salaJS = [7, 8, 8,7,10 ,6.5,4,10,7];
+const salaJava = [6,5,8,9,5,6];
+const salaPython = [7,3.5,8,9.5];
+
+function mediaSala(notasDaSala) {
+    const somaDasNotas = notasDaSala.reduce((acumulador, atual) => 
+    atual + acumulador, 0)
+    return somaDasNotas / notasDaSala.length
+}
+
+console.log(`Média da sala de Javascript ${mediaSala(salaJS)}`);
+console.log(`Média da sala de Javascript ${mediaSala(salaJava)}`);
+console.log(`Média da sala de Javascript ${mediaSala(salaPython)}`);
+
+//////////////////////////////////
+// media de um aluno com reduce //
+//////////////////////////////////
+const notas = [10, 6.5, 8, 7];
+
+const media = notas.reduce((acumulador, atual) => 
+atual + acumulador, 0) / notas.length
+
+console.log(media);
+
